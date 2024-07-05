0de2493c26368c5610aad21779c469668e53ae07
steamedEggMaster <cksdud5014@naver.com>
2024-07-05 13:46:50 +0900

    [main] first commit

---
+++ src/main/java/com/example/muddyteam_back/MuddyteamBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.muddyteam_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuddyteamBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuddyteamBackApplication.class, args);
+    }
+
+}
