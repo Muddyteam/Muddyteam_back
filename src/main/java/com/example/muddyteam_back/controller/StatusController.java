a478fda2ecaabbad0e3992ed2ac0a379eaf16d10
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-07-14 14:16:39 +0900

    feat : 카카오 로그인 구현
    서버 상태 체크용 컨트롤러 생성

---
+++ src/main/java/com/example/muddyteam_back/controller/StatusController.java
@@ -0,0 +1,16 @@
+package com.example.muddyteam_back.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(/server)
+public class StatusController {
+
+    @GetMapping("/status")
+    public ResponseEntity<String> checkStatus(){
+        return ResponseEntity.ok("OK");
+    }
+}
