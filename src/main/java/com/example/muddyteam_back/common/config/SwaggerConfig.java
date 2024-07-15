759a65d53b8da112df82f2e997eaa704c2b3b027
steamedEggMaster <cksdud5014@naver.com>
2024-07-15 21:56:53 +0900

    feat : 카카오 로그인 구현하기
    JWT가 적용된 Swagger 사용하기

--- src/main/java/com/example/muddyteam_back/common/config/SwaggerConfig.java
+++ src/main/java/com/example/muddyteam_back/common/config/SwaggerConfig.java
@@ -3,6 +3,9 @@ package com.example.muddyteam_back.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +14,14 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+                ))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .info(apiInfo());
     }
 
     private Info apiInfo() {
